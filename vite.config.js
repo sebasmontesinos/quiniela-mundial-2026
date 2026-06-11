d8b9cf43b5ef12f235186271e1bb94a95d1bfc45
Sebastian Montesinos <sebasmontesinos91@gmail.com>
2026-06-11 18:00:11 -0400

    feat: add PWA auto-update service worker

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
     react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        cleanupOutdatedCaches: true,
+        skipWaiting: true,
+        clientsClaim: true
+      },
+      manifest: false
+    }),
   ],
   server: {
     host: 'localhost',
