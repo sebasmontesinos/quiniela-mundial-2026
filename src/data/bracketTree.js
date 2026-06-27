4610093494df2ec28f3307bd64b0f4879efdd5bb
Sebastian Montesinos <sebasmontesinos91@gmail.com>
2026-06-27 01:41:35 -0400

    fix: correct R32 visual order in bracket columns

--- src/data/bracketTree.js
+++ src/data/bracketTree.js
@@ -23,31 +23,31 @@ export const BRACKET_TREE = {
 // The two sides of the bracket (for the ESPN-style symmetric layout).
 // Left side feeds SF_1, right side feeds SF_2.
 export const BRACKET_LEFT = {
-  r32: ['R32_1','R32_2','R32_3','R32_5','R32_9','R32_10','R32_11','R32_12'],
+  r32: ['R32_1','R32_3','R32_2','R32_5','R32_9','R32_10','R32_11','R32_12'],
   r16: [
-    {id:'R16_2', top:57},
-    {id:'R16_1', top:145},
-    {id:'R16_6', top:365},
-    {id:'R16_5', top:541},
+    {id:'R16_2', top:44},
+    {id:'R16_1', top:220},
+    {id:'R16_6', top:396},
+    {id:'R16_5', top:572},
   ],
   qf: [
-    {id:'QF_1', top:101},
-    {id:'QF_2', top:453},
+    {id:'QF_1', top:132},
+    {id:'QF_2', top:484},
   ],
-  sf: [{id:'SF_1', top:277}],
+  sf: [{id:'SF_1', top:308}],
 };
 
 export const BRACKET_RIGHT = {
-  r32: ['R32_4','R32_6','R32_7','R32_8','R32_13','R32_14','R32_15','R32_16'],
+  r32: ['R32_4','R32_6','R32_7','R32_8','R32_13','R32_15','R32_14','R32_16'],
   r16: [
-    {id:'R16_3', top:13},
-    {id:'R16_4', top:189},
-    {id:'R16_8', top:409},
-    {id:'R16_7', top:497},
+    {id:'R16_3', top:44},
+    {id:'R16_4', top:220},
+    {id:'R16_8', top:396},
+    {id:'R16_7', top:572},
   ],
   qf: [
-    {id:'QF_3', top:101},
-    {id:'QF_4', top:453},
+    {id:'QF_3', top:132},
+    {id:'QF_4', top:484},
   ],
-  sf: [{id:'SF_2', top:277}],
+  sf: [{id:'SF_2', top:308}],
 };
